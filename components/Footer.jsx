289944e0ab197de7b01baa7bac4777a7a2fdfc67
ammar <ammarmu007@gmail.com>
2025-08-02 16:46:00 +0500

    Save

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
+import { PhoneIcon } from "lucide-react";
 
 const Footer = () => {
   return (
@@ -9,10 +10,9 @@ const Footer = () => {
         <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            <strong>MY GAMES</strong> offers all types of video games in bahria
+            town phase 7. All kinds of gamings consoles like Ps4 , Ps5 , Xbox
+            360 , Ps Vita, Nintendo Switch etc are available.
           </p>
         </div>
 
@@ -48,8 +48,8 @@ const Footer = () => {
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+1-234-567-890</p>
-              <p>contact@greatstack.dev</p>
+              <p>+92-3325144172</p>
+              <p>jamshedisb786@gmail.com</p>
             </div>
           </div>
         </div>
