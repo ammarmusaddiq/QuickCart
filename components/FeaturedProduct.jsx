76cf4610ef73ef975a3d96112760bd160a1eb6a3
ammar <ammarmu007@gmail.com>
2025-08-05 18:44:05 +0500

    new

--- components/FeaturedProduct.jsx
+++ components/FeaturedProduct.jsx
@@ -28,7 +28,7 @@ const FeaturedProduct = () => {
     <div className="mt-14">
       <div className="flex flex-col items-center">
         <p className="text-3xl font-medium">Featured Products</p>
-        <div className="w-28 h-0.5 bg-orange-600 mt-2"></div>
+        <div className="w-28 h-0.5 bg-sony mt-2"></div>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-14 mt-12 md:px-14 px-4">
